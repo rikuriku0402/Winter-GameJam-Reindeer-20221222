fcdab8592c232b12046953afa88dcfc6285e3b10
miyagirei <ra.mu.umy243@gmail.com>
2022-12-22 10:42:49 +0900

    rule

--- Assets/ClafoMio/BackGroundScroll.cs
+++ Assets/ClafoMio/BackGroundScroll.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class BackGroundScroll : MonoBehaviour
 {
-    [SerializeField] float speed = 1;
-    [SerializeField] float maxSize;
+    [SerializeField] float _speed = 1;
+    [SerializeField] float _maxSize;
     private void Update()
     {
-        transform.position -= new Vector3(0, Time.deltaTime * speed);
+        transform.position -= new Vector3(0, Time.deltaTime * _speed);
 
-        if(transform.position.y <= -maxSize)
+        if(transform.position.y <= -_maxSize)
         {
-            transform.position = new Vector3(0, maxSize);
+            transform.position = new Vector3(0, _maxSize);
         }
     }
 }
